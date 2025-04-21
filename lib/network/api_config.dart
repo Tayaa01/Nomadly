986c47f47cd2b9ed943a6ab936611ebdc0713bc2
Tayaa01 <ayachi.mohamedtayaa@esprit.tn>
2025-04-21 15:05:08 +0100

    Update api_config.dart

--- lib/network/api_config.dart
+++ lib/network/api_config.dart
@@ -1,7 +1,6 @@
 class ApiConfig {
   // Main base URL for all services
-  static const String BASE_URL =
-      "http://192.168.1.15:3000";
+  static const String BASE_URL = "http://espritmobile.com/api";
 
   // Endpoints for Auth Service
   static const String REGISTER_ENDPOINT = "/auth/register";
@@ -29,15 +28,18 @@ class ApiConfig {
   static const String SAVINGS_BY_DAY_ENDPOINT = "/savings/by-day";
 
   // Add the new endpoint for image analysis without saving a transaction
-  static const String IMAGE_ANALYZE_CONVERT_ENDPOINT = "/image-currency/analyze-and-convert";
+  static const String IMAGE_ANALYZE_CONVERT_ENDPOINT =
+      "/image-currency/analyze-and-convert";
 
   // New endpoint for Travel Planner
   static const String TRAVEL_ITINERARY_ENDPOINT = "/travel/itinerary";
 
   // Travel Planner endpoints
   static const String TRAVEL_PLAN_ENDPOINT = "/travel-planner/plan";
-  static const String TRAVEL_GENERATE_PLAN_ENDPOINT = "/travel-planner/generate-plan";
-  static const String TRAVEL_GENERATE_BUDGET_PLAN_ENDPOINT = "/travel-planner/generate-budget-plan";
+  static const String TRAVEL_GENERATE_PLAN_ENDPOINT =
+      "/travel-planner/generate-plan";
+  static const String TRAVEL_GENERATE_BUDGET_PLAN_ENDPOINT =
+      "/travel-planner/generate-budget-plan";
 
   // Request timeouts
   static const int CONNECT_TIMEOUT = 30000; // 30 seconds
